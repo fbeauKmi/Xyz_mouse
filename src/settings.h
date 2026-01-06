0a69d66f9f1ba9ac1e6575806ff149de46f0ba3a
Frédéric Beaucamp <fbeaukmi@mailo.eu>
2026-01-06 02:28:42 +0100

    Fix settings.h , undocumented AxisZ

--- src/settings.h
+++ src/settings.h
@@ -13,7 +13,7 @@
 #define CLK 5       // CLK Encoder
 #define AxisY A2    // VRY Joystick
 #define AxisX A3    // VRX Joystick
-#define AxisZ A0    // VRZ Joystick, if define DT and CLK are not used
+//#define AxisZ A0    // VRZ Joystick, if define DT and CLK are not used
 #define joyButt A1  // SW Joystick
 #define button1 7   // Rear Button
 #define button2 8   // Rear Button
@@ -27,5 +27,8 @@
 #define BUTTONS_DEBOUNCE_DELAY      10 // Default 15, working values [5, 30]
 #define BUTTONS_CLICK_DELAY         60 // 
 #define ROTARYENCODER_DEBOUNCE_DELAY 2 // Default 2, working values [1, 10]
+#define DZ_X    1                   // deadzone scale by axis
+#define DZ_Y    1
+#define DZ_Z    4
 
 #endif
